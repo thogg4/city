5ee065fe092ad770e161e92d7990cbb89987e231
Tim <thogg4@gmail.com>
2012-06-01 11:15:16 -0600

    add syntax highlighting and auto completion

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,9 +2,13 @@ $(function() {
   $(".flash").delay(5000).fadeOut()
 
 
+  CodeMirror.fromTextArea($("textarea")[0], {
+    tabSize: 2,
+    matchBrackets: true,
+    workDelay: "500"
+  });
 
   zen_textarea.setup({
-    pretty_break: true,
     use_tab: true
   });
 
